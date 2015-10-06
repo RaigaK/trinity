65417468412800341c989b9e0a0d101f9c4d9b6b
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2015-10-06 11:35:03 +0000

    http://codecollaborator/ui#review:id=6755
    Fixing exposure docstring.
    
    Review: http://codecollaborator/ui#review:id=6755
    
    Binaries submitted in 968617
    Build time : 9 minutes 13.270000 seconds
    Branch : //eve/branches/development/MAINLINE/
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/Trinity/": change = 968609]

--- Eve/SpaceObject/Children/EveChildBillboard_Blue.cpp
+++ Eve/SpaceObject/Children/EveChildBillboard_Blue.cpp
@@ -23,7 +23,7 @@ const Be::ClassInfo* EveChildBillboard::ExposeToBlue()
 		MAP_ATTRIBUTE( "localTransform", m_localTransform, "", Be::READWRITE | Be::PERSIST )
 		MAP_ATTRIBUTE( "worldTransform", m_worldTransform, "", Be::READ )
 		MAP_ATTRIBUTE( "staticTransform", m_staticTransform, "Does local transform need to be rebuilt every frame.", Be::READWRITE | Be::PERSIST )
-		MAP_METHOD_AND_WRAP( "RebuildLocalTransform", RebuildLocalTransform, "Rebuilds local transform if useSRT is set." )
+		MAP_METHOD_AND_WRAP( "RebuildLocalTransform", RebuildLocalTransform, "Rebuilds local transform." )
 
     EXPOSURE_END()
 }
