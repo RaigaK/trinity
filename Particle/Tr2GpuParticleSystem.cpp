684c2c2c9188db872af0fc39398f09cfac05ce97
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2016-03-03 13:39:58 +0000

    Porting using //eve/development/MAINLINE<-//eve/staging/2016-COLIN at head (1014987)
    Binaries submitted in 1015047
    Build time : 21 minutes 30.425000 seconds
    Branch : //eve/branches/development/MAINLINE/
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/Trinity/": change = 1015014]

--- Particle/Tr2GpuParticleSystem.cpp
+++ Particle/Tr2GpuParticleSystem.cpp
@@ -912,6 +912,7 @@ void Tr2GpuParticleSystem::Sort( Tr2RenderContext& renderContext )
 	Tr2GpuTimerALContext ctx( m_sortTimer, renderContext );
 
 	renderContext.m_esm.UnsetAllTextures();
+	renderContext.CopyBufferCounter( *m_sortParameters, 0, *m_visibleList );
 	Tr2Renderer::RunComputeShader( m_setSortParameters, 1, 1, 1, renderContext );
 	Tr2Renderer::RunComputeShaderIndirect( m_sort, *m_sortParameters, 0, renderContext );
 
